ca270cd75c418e0540ee53082026f28acfce1d9e
maaryhabad <maryh.abad@gmail.com>
2017-01-21 04:36:48 -0200

    UI

---
+++ Assets/scripts/char.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class character : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void moveTo (Vector3 position)
+    {
+        transform.position = position;             
+    }
+
+    IEnumerator MoveFromTo(Vector3 a, Vector3 b, float speed) {
+         float step = (speed / (a - b).magnitude) * Time.fixedDeltaTime;
+         float t = 0;
+         while (t <= 1.0f) {
+             t += step; // Goes from 0 to 1, incrementing by step each time
+             yield return new WaitForFixedUpdate();         // Leave the routine and return here in the next frame
+         }
+         transform.position = b;
+ }
+}
---
+++ Assets/scripts/skill.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class skill : MonoBehaviour {
+    int actionPoint;
+    public Text error;
+    // Use this for initialization
+    void Start()
+    {
+        actionPoint = 2;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (actionPoint < 0)
+        {
+            error.gameObject.SetActive(true);
+        }
+      
+	}
+    private void OnMouseDown()
+    {
+        
+    }
+}
